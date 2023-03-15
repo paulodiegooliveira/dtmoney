ce2d9f14e4e66ae831f3ae771e1324e184f529fd
Diego Oliveira <paulodiego.oliveira@gmail.com>
2023-03-15 20:00:02 +0000

    Api transactions.php

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://www.afeorocket.com.br/api/transactions.php",
+  baseURL: "https://www.afeorocket.com.br/api",
   // baseURL: "http://localhost:3000/api",
 });
